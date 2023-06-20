ff5cab4d5bc0d3e12361ba0c6c1dfe581c388384
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-06-20 22:53:52 +0530

    Initial commit

---
+++ deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,25 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deployer } = await getNamedAccounts()
+    const { deploy, log } = deployments
+
+    const args = []
+    log("________________________________________________________________")
+    const nftMarketplace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(nftMarketplace.address, args)
+        log("______________________________Verified_______________________________")
+    }
+}
+
+module.exports.tags = ["all", "nftmarketplace"]
